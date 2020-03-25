981d92464be845d10e9f4d308a8d6cb4e13dcbe5
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-03-25 13:02:20 +0100

    Update SecurityDeserializationTestCase.java

--- modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
+++ modules/src/main/java/org/jboss/additional/testsuite/jdkall/present/security/other/SecurityDeserializationTestCase.java
@@ -362,7 +362,7 @@ public class SecurityDeserializationTestCase {
             assertTrue("Fail because of security issues...",e.getMessage().contains("prevented for security reasons"));
         }
     }
-    
+
     @ATTest({"modules/testcases/jdkAll/Wildfly/security/src/main/java#20.0.0.Beta1","modules/testcases/jdkAll/Eap72x-Proposed/security/src/main/java#7.2.9","modules/testcases/jdkAll/Eap72x/security/src/main/java#7.2.9","modules/testcases/jdkAll/Eap7/security/src/main/java#7.3.1.GA"})
     public void testSecuirtyDatabind17() throws Exception {
 
