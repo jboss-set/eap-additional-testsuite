28396db335500079e2503f03d45a2867b4e1dab3
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-05-06 20:28:25 +0200

    Minor update

--- modules/src2/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/spec/SpecTestCase.java
+++ modules/src2/main/java/org/jboss/additional/testsuite/jdkall/present/ejb/spec/SpecTestCase.java
@@ -41,7 +41,7 @@ public class SpecTestCase {
         FileInputStream inputStream = new FileInputStream(path + "../" + serverLogPath);
         try {
             String everything = IOUtils.toString(inputStream);
-            assertFalse("SessionBeans should have only one of the following types : Stateful, Stateless, Singleton", everything.contains("SessionBeans should have only one of the following types : Stateful, Stateless, Singleton"));
+            assertFalse("Session EJBs should have only one of the following types : Stateful, Stateless, Singleton", everything.contains("Session EJBs should have only one of the following types : Stateful, Stateless, Singleton"));
         } finally {
             inputStream.close();
         }
